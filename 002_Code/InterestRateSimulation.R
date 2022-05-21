6fb2e5536f8ec17319ec8d27ce9a2c2122e47f18
PIOTR MIKLER <piotr.mikler1997@gmail.com>
2022-05-21 19:16:10 +0200

    Add code structure

---
+++ 002_Code/InterestRateSimulation.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+
+simulate_gbm <- function(r0 = 0.05, sigma = 0.1, mu = 0.01,
+                         T = 5.0, nsteps=100, npaths=4){
+  # funkcja symulujaca stope procentowa Blackiem-Scholesem
+  dt <- T/nsteps
+  Z <- array(rnorm(n = nsteps*npaths, mean = 0, sd = 1),
+             dim=c(nsteps, npaths))*sigma*dt + (mu - sigma^2/2)*dt
+  Xt <- apply(Z, 2, cumsum)
+  
+  rt <- r0*exp(Xt)
+  return(rbind(rep(r0, npaths), rt))
+}
