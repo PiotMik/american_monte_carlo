ea6adafca0dc46f4af337128eb54cd777a06c80d
PIOTR MIKLER <piotr.mikler1997@gmail.com>
2022-06-12 05:26:19 +0200

    Add: Plots for Tsitsiklis and for comparison

--- 002_Code/plots.py
+++ 002_Code/plots.py
@@ -1,8 +1,14 @@
-from utils import plot_distribution, plot_early_exercise_boundary, gbm
+from utils import gbm
 from derivatives import AmericanOption, InterestRateSwap
 from tilley import tilley_price
+from tsitsiklis import tsitsiklis_price
 import numpy as np
-from scipy.stats import norm
+from sklearn.linear_model import LassoCV, LinearRegression
+from sklearn.preprocessing import StandardScaler, KBinsDiscretizer
+from mlinsights.mlmodel import PiecewiseRegressor
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import Pipeline
+
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -21,18 +27,24 @@ def plot_interest_rate_distribution():
     r0 = 0.01
     T = 5.0
 
-    paths = 10000
-    time_steps = 21
+    paths = 1000
+    time_steps = 51
 
     rf_sim = gbm(r0=r0, sigma=sigma, mu=rfr, steps=time_steps, paths=paths).T
 
-    derivative = AmericanOption(notional=1_000, strike=0.01, expiry=T, option_type="put")
-    plot_distribution(rf_sim, T=derivative.expiry, epe=False,
-                      quantiles=[0.025, 0.1, 0.25, 0.5, 0.75, 0.9, 0.975],
-                      saveas=f"InterestRate.pdf")
+    qs = [0.025, 0.1, 0.25, 0.5, 0.75, 0.9, 0.975]
+    df_quantiles = pd.DataFrame(np.quantile(rf_sim, qs, axis=0).T,
+                                columns=['$q_{' + f'{q:.3f}' + "}$" for q in qs],
+                                index=np.linspace(0, T, rf_sim.shape[1]))
+    fig, ax = plt.subplots()
+    ax.plot(df_quantiles.index, rf_sim.T, color='grey', alpha=0.4)
+    df_quantiles.plot(ax=ax, colormap='jet')
+    fig.savefig(f"003_Outputs/InterestRate.pdf")
+    plt.show()
+    plt.close(fig)
 
 
-def plot_tilley_exposures():
+def plot_exposures():
     rfr = 0.05
     sigma = 0.2
     r0 = 0.01
@@ -47,20 +59,34 @@ def plot_tilley_exposures():
                    InterestRateSwap(notional=1_000, fixed_rate=0.010501, expiry=T, position='receiver',
                                     payment_freq_pa=4.0)]
 
-    alpha = [0.7]
-    for derivative in derivatives:
-        for a in alpha:
-            P = int(paths**a)
-            Q = int(paths/P)
+    for derivative, alpha in zip(derivatives, [0.5, 0.65]):
+        P = int(paths**alpha)
+        Q = int(paths/P)
 
-            price_option, V_option, ex_bound = tilley_price(derivative=derivative,
-                                                            time_steps=time_steps, P=P, Q=Q,
-                                                            rf_sim=rf_sim[:P*Q, :], rfr=rfr)
-            plot_distribution(V_option, quantiles=[0.025, 0.975], epe=True, T=derivative.expiry,
-                              saveas=f"Tilley_Exposure_{type(derivative).__name__}.pdf")
+        price_tilley, V_tilley, _ = tilley_price(derivative=derivative,
+                                                 time_steps=time_steps, P=P, Q=Q,
+                                                 rf_sim=rf_sim[:P*Q, :], rfr=rfr)
+        price_tsitsiklis, V_tsitsiklis, _ = tsitsiklis_price(derivative=derivative,
+                                                             rf_sim=rf_sim,
+                                                             rfr=rfr, time_steps=time_steps)
+
+        fig, ax = plt.subplots()
+        quantiles = [0.025, 0.1, 0.5, 0.9, 0.975]
+        for V_estimate, name, line in zip([V_tilley, V_tsitsiklis], ['Tilley', 'Tsitsiklis'], ['-', '--']):
+            df_quantiles = pd.DataFrame(np.quantile(V_estimate, quantiles, axis=0).T,
+                                        columns=[name + ' $q_{' + f'{q:.3f}' + "}$" for q in quantiles],
+                                        index=np.linspace(0, T, V_estimate.shape[1]))
+
+            df_quantiles.plot(ax=ax, linestyle=line, colormap='jet')
+            #df_quantiles['EPE'] = np.maximum(V_estimate, 0).mean(axis=0).T
+            #df_quantiles['EPE'].plot(ax=ax, legend=True)
+
+        plt.show()
+        fig.savefig(f"003_Outputs/Exposures_{type(derivative).__name__}.pdf")
+        plt.close(fig)
 
 
-def plot_tilley_early_exercise():
+def plot_epe():
     rfr = 0.05
     sigma = 0.2
     r0 = 0.01
@@ -71,17 +97,75 @@ def plot_tilley_early_exercise():
 
     rf_sim = gbm(r0=r0, sigma=sigma, mu=rfr, steps=time_steps, paths=paths).T
 
+    derivatives = [AmericanOption(notional=1_000, strike=0.01, expiry=T, option_type="put"),
+                   InterestRateSwap(notional=1_000, fixed_rate=0.010501, expiry=T, position='receiver',
+                                    payment_freq_pa=4.0)]
+
+    for derivative, alpha in zip(derivatives, [0.5, 0.65]):
+        P = int(paths**alpha)
+        Q = int(paths/P)
+
+        price_tilley, V_tilley, _ = tilley_price(derivative=derivative,
+                                                 time_steps=time_steps, P=P, Q=Q,
+                                                 rf_sim=rf_sim[:P*Q, :], rfr=rfr)
+        price_tsitsiklis, V_tsitsiklis, _ = tsitsiklis_price(derivative=derivative,
+                                                             rf_sim=rf_sim,
+                                                             rfr=rfr, time_steps=time_steps)
+        fig, ax = plt.subplots()
+        quantiles = [0.025, 0.1, 0.5, 0.9, 0.975]
+        for V_estimate, name, line in zip([V_tilley, V_tsitsiklis], ['Tilley', 'Tsitsiklis'], ['-', '--']):
+            df_quantiles = pd.DataFrame(np.quantile(V_estimate, quantiles, axis=0).T,
+                                        columns=[name + ' $q_{' + f'{q:.3f}' + "}$" for q in quantiles],
+                                        index=np.linspace(0, T, V_estimate.shape[1]))
+
+            df_quantiles[f'{name} EPE'] = np.maximum(V_estimate, 0).mean(axis=0).T
+            df_quantiles[f'{name} EPE'].plot(ax=ax, legend=True, linestyle=line)
+
+        plt.show()
+        fig.savefig(f"003_Outputs/EPEs_{type(derivative).__name__}.pdf")
+        plt.close(fig)
+
+
+def plot_early_exercise():
+    rfr = 0.05
+    sigma = 0.2
+    r0 = 0.01
+    T = 5.0
+
+    paths = 5000
+    time_steps = 51
+
+    rf_sim = gbm(r0=r0, sigma=sigma, mu=rfr, steps=time_steps, paths=paths).T
+
     derivative = AmericanOption(notional=1_000, strike=0.01, expiry=T, option_type="put")
-    alpha = 0.7
+    alpha = 0.5
     P = int(paths ** alpha)
     Q = int(paths / P)
 
-    price_option, V_option, ex_bound = tilley_price(derivative=derivative,
-                                                    time_steps=time_steps, P=P, Q=Q,
-                                                    rf_sim=rf_sim[:P * Q, :], rfr=rfr)
-    plot_early_exercise_boundary(rf_sim, ex_bound=ex_bound, T = derivative.expiry,
-                                 quantiles=[0.25, 0.75],
-                                 saveas=f"Tilley_EarlyExercise_{type(derivative).__name__}.pdf")
+    _, V_tilley, eeb_tilley = tilley_price(derivative=derivative,
+                                           time_steps=time_steps, P=P, Q=Q,
+                                           rf_sim=rf_sim[:P * Q, :], rfr=rfr)
+    _, V_tsitsiklis, eeb_tsitsiklis = tsitsiklis_price(derivative=derivative,
+                                                       rf_sim=rf_sim,
+                                                       rfr=rfr, time_steps=time_steps)
+
+    fig, ax = plt.subplots()
+    ax.plot(np.linspace(0, T, V_tsitsiklis.shape[1]),
+            rf_sim[np.random.choice(range(rf_sim.shape[0]), 1000), :].T,
+            color='grey', alpha=0.2)
+    ax.set_ylabel('$r_t$')
+    ax.set_xlabel('$t$')
+
+    for V_estimate, eeb_estimate, name in zip([V_tilley, V_tsitsiklis],
+                                              [eeb_tilley, eeb_tsitsiklis],
+                                              ['Tilley', 'Tsitsiklis']):
+        eeb_estimate[eeb_estimate > rf_sim.mean(axis=0)] = np.nan
+        eeb = pd.DataFrame(eeb_estimate, index=np.linspace(0, T, V_estimate.shape[1]),
+                           columns=[f'{name} Early Exercise Boundary']).replace(0.0, np.nan)
+        eeb.plot(ax=ax)
+    plt.show()
+    fig.savefig(f"003_Outputs/EarlyExerciseBoundaries_AmericanOption.pdf")
+    plt.close(fig)
 
 
 def plot_tilley_alpha_dependency():
@@ -122,9 +206,72 @@ def plot_tilley_alpha_dependency():
         plt.close(fig)
 
 
+def plot_tsitsiklis_regressions():
+    rfr = 0.05
+    sigma = 0.2
+    r0 = 0.01
+    T = 5.0
+
+    paths = 10000
+    time_steps = 21
+
+    rf_sim = gbm(r0=r0, sigma=sigma, mu=rfr, steps=time_steps, paths=paths).T
+    derivative = AmericanOption(notional=1_000, strike=0.01, expiry=T, option_type="put")
+
+    V = np.zeros_like(rf_sim)
+    cf = np.zeros_like(rf_sim)
+    T = derivative.expiry
+    dt = T / (time_steps - 1)
+
+    i = -1
+    cf[:, i] = derivative.intrinsic_value(rf_sim, i)
+    V[:, i] = cf[:, i]
+
+    i = -2
+    X = rf_sim[:, i].ravel().reshape(-1, 1)
+    Y = (V[:, i + 1].ravel().reshape(-1, 1) + derivative.cashflow(rf_sim, i).reshape(-1, 1))*np.exp(-rfr*dt)
+
+    models = [Pipeline([('scaler', StandardScaler()),
+                        ('poly', PolynomialFeatures(1)),
+                        ('regressor', LinearRegression())]),
+              Pipeline([('scaler', StandardScaler()),
+                        ('poly', PolynomialFeatures(2)),
+                        ('regressor', LinearRegression())]),
+              Pipeline([('scaler', StandardScaler()),
+                        ('poly', PolynomialFeatures(3)),
+                        ('regressor', LinearRegression())]),
+              Pipeline([('scaler', StandardScaler()),
+                        ('poly', PolynomialFeatures(4)),
+                        ('regressor', LinearRegression())]),
+              Pipeline([('scaler', StandardScaler()),
+                        ('poly', PolynomialFeatures(4)),
+                        ('regressor', LassoCV())]),
+              Pipeline([('scaler', StandardScaler()),
+                        ('poly', PolynomialFeatures(2)),
+                        ('regressor', PiecewiseRegressor(verbose=True,
+                                                         binner=KBinsDiscretizer(n_bins=2)))])]
+    # Value of continuation
+    fig, ax = plt.subplots(2, 3, figsize=(8, 6))
+    names = ['LinReg ord.1', 'LinReg ord.2', 'LinReg ord.3', 'LinReg ord.4',
+             'Lasso ord.4', '2-piece LinReg ord.2']
+    for axis, model, name in zip(ax.ravel(), models, names):
+        model.fit(X=X, y=Y)
+        axis.scatter(X, Y, marker='+')
+        axis.plot(sorted(X), model.predict(sorted(X)).ravel(), color='crimson')
+        axis.set_title(name)
+
+    plt.tight_layout()
+    plt.show()
+    fig.savefig("003_Outputs/Regressions_Tsitsiklis.pdf")
+    plt.close(fig)
+
+
 if __name__ == "__main__":
 
-    plot_interest_rate_distribution()
-    plot_tilley_early_exercise()
-    plot_tilley_exposures()
-    plot_tilley_alpha_dependency()
+    # plot_exposures()
+    plot_epe()
+
+    #plot_interest_rate_distribution()
+    #plot_early_exercise()
+    #plot_tilley_alpha_dependency()
+    #plot_tsitsiklis_regressions()
--- 002_Code/tsitsiklis.py
+++ 002_Code/tsitsiklis.py
@@ -1,49 +1,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import norm
 from sklearn.linear_model import LassoCV, LinearRegression
 from sklearn.preprocessing import StandardScaler, KBinsDiscretizer
 from mlinsights.mlmodel import PiecewiseRegressor
-from sklearn.tree import DecisionTreeRegressor
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import Pipeline
 from derivatives import InterestRateSwap, AmericanOption
 import seaborn as sns
-from utils import gbm, plot_distribution
+from utils import gbm, plot_distribution, plot_early_exercise_boundary
 sns.set()
 
-if __name__ == '__main__':
 
-    rfr = 0.05
-    sigma = 0.2
-    r0 = 0.01
-    T = 5.0
-
-    paths = 1000
-    time_steps = 21
-    dt = T/(time_steps - 1)
-
-    rf_sim = gbm(r0=r0, sigma=sigma, mu=rfr, steps=time_steps, paths=paths).T
-
-    derivatives = [AmericanOption(notional=1_000, strike=0.01, expiry=T, option_type="put"),
-                   InterestRateSwap(notional=1_000, fixed_rate=0.010501, expiry=T, position='receiver',
-                                    payment_freq_pa=4.0)]
-    derivative = derivatives[1]
-    plt.plot(rf_sim.T)
+def tsitsiklis_price(rfr, time_steps, derivative, rf_sim):
 
     V = np.zeros_like(rf_sim)
     cf = np.zeros_like(rf_sim)
-    disc = np.full_like(rf_sim, fill_value=np.exp(rfr * dt)).cumprod(axis=1)
+    early_exercise_boundary = np.full_like(V[0, :], fill_value=np.nan)
+    T = derivative.expiry
+    dt = T / (time_steps - 1)
+
     for i in range(-1, -time_steps, -1):
         if i == -1:
             cf[:, i] = derivative.intrinsic_value(rf_sim, i)
             V[:, i] = cf[:, i]
         else:
-            disc_ = disc/disc[0, i]
             e = derivative.intrinsic_value(rf_sim, i)
             X = rf_sim[:, i].ravel().reshape(-1, 1)
-            Y = V[:, i + 1].ravel().reshape(-1, 1)*np.exp(-rfr*dt)
-
+            Y = (V[:, i + 1].ravel().reshape(-1, 1) + derivative.cashflow(rf_sim, i).reshape(-1, 1))*np.exp(-rfr*dt)
 
             #regressor = LassoCV()
             regressor = PiecewiseRegressor(verbose=True,
@@ -58,23 +41,37 @@ if __name__ == '__main__':
 
             if derivative.early_exercisable:
                 V[:, i] = np.maximum(c, e)
+                rf_at_ex = rf_sim[e >= c, i]
+                if len(rf_at_ex):
+                    early_exercise_boundary[i] = np.max(rf_at_ex) if derivative.option_type == "put" else np.min(rf_at_ex)
             else:
-                V[:, i] = c + derivative.cashflow(rf_sim, i)
+                V[:, i] = c
 
-            if True:
-                fig, ax = plt.subplots()
-                ax.scatter(X, Y)
-                ax.scatter(X, model.predict(X).ravel(), color='red')
-                plt.show()
     V[:, 0] = V[:, 1].mean()*np.exp(-rfr*dt)
-    plot_distribution(V=V, quantiles=[0.025, 0.25, 0.5, 0.75, 0.975], T=derivative.expiry,
+
+    return V[0, 0], V, early_exercise_boundary
+
+
+if __name__ == '__main__':
+
+    rfr = 0.05
+    sigma = 0.2
+    r0 = 0.01
+    T = 5.0
+
+    paths = 10000
+    time_steps = 21
+    dt = T/(time_steps - 1)
+
+    rf_sim = gbm(r0=r0, sigma=sigma, mu=rfr, steps=time_steps, paths=paths).T
+
+    derivatives = [AmericanOption(notional=1_000, strike=0.01, expiry=T, option_type="put"),
+                   InterestRateSwap(notional=1_000, fixed_rate=0.010501, expiry=T, position='receiver',
+                                    payment_freq_pa=4.0)]
+    derivative = derivatives[0]
+
+    price, V, early_exercise_boundary = tsitsiklis_price(rfr, time_steps, derivative, rf_sim)
+    plot_distribution(V=V, quantiles=[0.025, 0.975], epe=True, T=derivative.expiry,
                       saveas=f"Tsitsiklis_Exposure_{type(derivative).__name__}.pdf")
-
-    price = V[0, 0]
-
-    d1 = (np.log(r0 / derivative.strike) + (rfr - sigma ** 2 / 2) * derivative.expiry) / (sigma * np.sqrt(derivative.expiry))
-    d2 = d1 - sigma * np.sqrt(derivative.expiry)
-    bs_price = norm.cdf(d1) * r0 - norm.cdf(d2) * derivative.strike * np.exp(-rfr * derivative.expiry)
-
-    print(f"American Call Price: {price:.4f}")
-    print(f"European Call Price: {bs_price:.4f}")
+    #plot_early_exercise_boundary(rf_sim, T=derivative.expiry, ex_bound=early_exercise_boundary, quantiles=[0.025, 0.975],
+    #                             saveas=f"Tsitsiklis_EarlyExercise_AmericanOption.pdf")
